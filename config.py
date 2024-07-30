ac68ac6f725b9f95985b8ea36a6ea035d7a2e1b4
FGsfeajerbFsfwdkotevkfcjod1 <hookmehost+2@gmail.com>
2024-07-30 22:42:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    API_ID = int(os.getenv("API_ID", ))
-    API_HASH = os.getenv("API_HASH", '')
-    BOT_TOKEN = os.getenv("BOT_TOKEN", '')
+    API_ID = int(os.getenv("API_ID", 28157070))
+    API_HASH = os.getenv("API_HASH", '9078ae3b29412c4c1220e631edc5ed77')
+    BOT_TOKEN = os.getenv("BOT_TOKEN", '7195643309:AAGGsnorpch1N8I1gMa_IYudiQzGQhkPEXk')
